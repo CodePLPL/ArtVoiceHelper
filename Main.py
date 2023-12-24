70a7f030640bfbb74d8288c8dc5d3f9a44e47627
Code++ <MakarMainMatievskiy@yandex.ru>
2023-12-24 19:13:10 +0300

    24.12.23

--- Main.py
+++ Main.py
@@ -3,9 +3,12 @@ import keyboard
 import os
 import json ##Импортируемые библиотеки
 import webbrowser
+import datetime
+import requests
 from gtts import gTTS
 from pydub.playback import play
 from pydub import AudioSegment
+from datetime import datetime
 
 r = sr.Recognizer() ## Подключение обработчика
 mic = sr.Microphone() ## Подключение микрофона
@@ -29,6 +32,7 @@ tts = gTTS("Я приветствую вас, чем я могу вам помо
 tts.save('hello.mp3')
 HelloA = AudioSegment.from_mp3('hello.mp3')
 play(HelloA)
+Recs = AudioSegment.from_mp3('RecS.mp3')
 
 
 
@@ -61,7 +65,7 @@ while True:
         if voicename == "Привет арт":
             with mic as source:
                 r.adjust_for_ambient_noise(source)
-                print("Запись Пошла")
+                play(Recs)
                 audio = r.listen(source)
             text = r.recognize_google(audio, language='ru-RU')
             print(text)
@@ -76,7 +80,25 @@ while True:
             elif text == "открой github":
                 webbrowser.open("https://github.com/")
             elif text == "Очисти логи":
-                os.system('cls')                
+                os.system('cls')     
+            elif text == "Какой сегодня день":
+                current_datetime = datetime.now().date()
+                current_datetime_str = str(current_datetime)
+                tts = gTTS(current_datetime_str, lang='ru')
+                tts.save('datetime.mp3')
+                DataTime = AudioSegment.from_mp3('datetime.mp3')
+                play(DataTime)
+            elif text == "статус сети":
+                if requests.get('https://www.youtube.com/').ok:
+                    tts = gTTS("Соединение стабильное", lang='ru')
+                    tts.save('StatusOk.mp3')
+                    StatusWeb = AudioSegment.from_mp3('StatusOk.mp3')
+                    play(StatusWeb)
+                else:
+                    tts = gTTS("Соединение нестабильное", lang='ru')
+                    tts.save('StatusNO.mp3')
+                    StatusWeb = AudioSegment.from_mp3('StatusNO.mp3')
+                    play(StatusWeb)
             else:
                 print("К сожалению я не понял вас")
                 continue
@@ -88,3 +110,10 @@ while True:
 # Очистка лишних данных     
 if os.path.exists('hello.mp3'):
     os.remove('hello.mp3')
+if os.path.exists('datetime.mp3'):
+    os.remove('datetime.mp3')
+if os.path.exists('StatusOk.mp3'):
+    os.remove('StatusOk.mp3')
+if os.path.exists('StatusNO.mp3'):
+    os.remove('StatusNO.mp3')
+
