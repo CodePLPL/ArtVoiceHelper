098decb544c71751ce5777964040d0ad49fcad94
Code++ <MakarMainMatievskiy@yandex.ru>
2024-03-02 21:54:06 +0300

    pre-Realise-2

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import json
 import webbrowser
 import datetime
 import requests
-import openai
+from openai import OpenAI
 from gtts import gTTS
 from pydub.playback import play
 from pydub import AudioSegment
@@ -15,6 +15,24 @@ from datetime import datetime
 r = sr.Recognizer() ## Подключение обработчика
 mic = sr.Microphone() ## Подключение микрофона
 sr.LANGUAGE = 'ru-RU' ## Язык обработки
+
+def console_Art():
+    print(" ##   ####   ####")
+    print("#  #  #  #    # ")
+    print("####  ###     # ")
+    print("#  #  #  #    # ")
+
+console_Art()
+
+print("\033[33m" + "ART (automator of routine tasks)" + "\033[0m")
+print("\033[33m" + "Это мой школьный проект. Я постарался в нем показать мои способности в программировании. Кроме того, это мой первый проект. Буду рад, если вы оцените его" + "\033[0m")
+print("\033[33m" + "Для запуска программы у вас должен быть установлен python, если у вас возникают проблемы в работе программы то возможно вы словили баг. Если у вас возникают проблемы при работе с ии, то вероятнее всего сервера OpenAi не доступны" + "\033[0m")
+print("\033[33m" + "This is my school project. I tried to show my programming abilities in it. Besides, this is my first project. I will be glad if you rate it" + "\033[0m")
+print("\033[33m" + "To run the program, you must have python installed; if you have problems with the program, you may have caught a bug. If you have problems working with AI, then most likely the OpenAi servers are not available" + "\033[0m")
+print("Вы можете поддержать проект криптой BTC:1CVLhwWgHxoEciQTjZsxTQh6yd92PAmpjp, ETH:0x883909cdBD6A9B6f6CDBfa9118A45E41c3b06fBc;\nYou can support the project with crypto: BTC:1CVLhwWgHxoEciQTjZsxTQh6yd92PAmpjp, ETH:0x883909cdBD6A9B6f6CDBfa9118A45E41c3b06fBc")
+print("__________________________________________")
+
+print("\033[31m" + "Для обычного вызова нажмите 'left alt', для ии вызова нажмите 'left ctrl', чтобы выйти из программы скажите 'остановка' или нажмите 'Esc';\nFor a normal call, press 'left alt', for a normal call, press 'left ctrl', to exit the program, say 'stop' or press 'Esc'" + "\033[0m")
 
 if os.path.exists('data.json'):
     with open("data.json", "r") as read_file:
@@ -27,17 +45,8 @@ else:
         json.dump(data, write_file)
 ## Файл данных
         
-openai.api_key = data['ApiKey'] 
-model_engine = "text-davinci-003"
-max_tokens = 128
-# Ввод ключа OpenAI
-
-tts = gTTS("Я приветствую вас, чем я могу вам помочь", lang='ru')
-tts.save('hello.mp3')
-HelloA = AudioSegment.from_mp3('hello.mp3')
-play(HelloA)
 Recs = AudioSegment.from_mp3('RecS.mp3')
-
+RecEr = AudioSegment.from_mp3('RecEr.mp3')
 while True:
     try:
         if keyboard.is_pressed('left alt'):
@@ -80,10 +89,34 @@ while True:
                     play(StatusWeb)
             else:
                 print("К сожалению я не понял вас")
-                continue
+                play(RecEr)
+                continue  
+        elif keyboard.is_pressed('left ctrl'):
+            
+            with mic as source:
+                r.adjust_for_ambient_noise(source)
+                play(Recs)
+                audio = r.listen(source)
+            text = r.recognize_google(audio, language='ru-RU')
+            print(text)
+            
+            client = OpenAI(api_key = data['ApiKey'])
+
+            stream = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": text}],
+                stream=True,
+            )
+            for chunk in stream:
+                if chunk.choices[0].delta.content is not None:
+                    print(chunk.choices[0].delta.content, end="")
+            print("/>>")  
+        elif keyboard.is_pressed('esc'):
+            break            
         else:
             continue
     except:
+        play(RecEr)
         continue
 
 # Очистка лишних данных     
